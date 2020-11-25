0d6e37838910d6dff6a23608f8e564b32cae1f10
rui.ribeiro <>
2020-11-25 22:32:44 +0000

    feat: update dashboard with new line section components

--- src/components/feature/Dashboard/Dashboard.tsx
+++ src/components/feature/Dashboard/Dashboard.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Header } from '../../shared';
+import LineSection from './components/LineSection/LineSection';
 
 const Dashboard = () => {
   return (
     <div>
       <Header onSearchChange={(searchValue) => console.log(searchValue)} />
-      this is dashboard
+      <div>
+        <LineSection title='Trending Users'>this are the top 3 trending users</LineSection>
+        <LineSection title='Most Active Users'>this are the top 3 most active users</LineSection>
+        <LineSection title='Top Repositories'>this are the top 3 repositories</LineSection>
+      </div>
     </div>
   );
 };
