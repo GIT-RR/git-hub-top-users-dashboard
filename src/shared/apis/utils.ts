da8b047c9647fd2e1c5073c91b0a47b0283aeb12
rui.ribeiro <>
2020-11-28 01:10:03 +0000

    feat: integration of search input with apis

--- src/shared/apis/utils.ts
+++ src/shared/apis/utils.ts
@@ -1,8 +1,8 @@
 // Git Api is secured. Only allows x requests per minute. Use your own git credentials to increase the requests per minute. Update gitEmail and gitPassword.
 // Error Message: "API rate limit exceeded for 85.244.138.154. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)"
 // NOTE: DONT COMMIT YOU EMAIL OR PASSWORD !!!
-const gitEmail = 'ribeiro.ruifigueiredo@gmail.com';
-const gitPassword = 'kawaKLX125';
+const gitEmail = '';
+const gitPassword = '';
 
 export const getGitCredentials = () => {
   const useGitCredentials = gitEmail && gitPassword;
